26aeaa150e2f9daf04ddec4273c5c6a4fe4aed86
KJM-BlueVisor <kjm@bluevisor.kr>
2025-07-25 16:28:01 +0900

    필요한 의존성 추가 및 SearchScreen 작업 시작

--- feature-search/src/main/java/kr/jm/feature_search/SearchScreen.kt
+++ feature-search/src/main/java/kr/jm/feature_search/SearchScreen.kt
@@ -1,1 +1,48 @@
 package kr.jm.feature_search
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SearchScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("하차 알리미🔔")
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            TextField(
+                value = "",
+                onValueChange = {}
+            )
+            Button(onClick = {}) {
+
+            }
+        }
+        Card(
+
+        ) {  }
+
+        LazyColumn {  }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun SearchScreenPreview() {
+    SearchScreen()
+}
