5ba4bcb4d39bab0a3f6debc06d0c373bea74abb8
KJM-BlueVisor <kjm@bluevisor.kr>
2025-07-30 16:05:01 +0900

    State 생성

--- feature-search/src/main/java/kr/jm/feature_search/SearchScreen.kt
+++ feature-search/src/main/java/kr/jm/feature_search/SearchScreen.kt
@@ -1,8 +1,8 @@
 package kr.jm.feature_search
 
-import android.graphics.drawable.shapes.Shape
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -16,27 +16,34 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-fun SearchScreen() {
+fun SearchScreen(
+    searchViewModel: SearchViewModel = hiltViewModel()
+) {
+    val searchScreenState by searchViewModel.searchScreenState
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xFFF5F5F5))
             .padding(8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
     ) {
         Text("하차 알리미🔔")
         Row(
@@ -95,7 +102,15 @@ fun SearchScreen() {
             }
         }
 
-
+        DropdownMenu(
+            expanded = searchScreenState.dropDownExpanded,
+            onDismissRequest = {}
+        ) {
+            DropdownMenuItem(
+                text = { Text("노선 선택")},
+                onClick = {}
+            )
+        }
 
         LazyColumn { }
     }
