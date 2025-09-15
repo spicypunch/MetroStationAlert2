f9d78d148a6e5dc54080fccb7c09c58f776070ef
KJM-BlueVisor <kjm@bluevisor.kr>
2025-09-15 11:36:44 +0900

    화면 데이터 연동

--- feature-bookmark/src/main/java/kr/jm/feature_bookmark/BookmarkScreen.kt
+++ feature-bookmark/src/main/java/kr/jm/feature_bookmark/BookmarkScreen.kt
@@ -1,24 +1,37 @@
 package kr.jm.feature_bookmark
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kr.jm.common_ui.component.CommonStationCard
 
 @Composable
 fun BookmarkScreen(
     bookmarkViewModel: BookmarkViewModel = hiltViewModel()
 ) {
+    val bookmarkScreenState by bookmarkViewModel.uiState.collectAsStateWithLifecycle()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -30,11 +43,25 @@ fun BookmarkScreen(
             "지하철 도착 정보🚊",
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold,
-            modifier = Modifier.clickable { bookmarkViewModel.getSubwayArrivalTime() })
-        CommonStationCard(
-            primaryText = "야탑역",
-            secondText = "도착: 3분전",
-            expandableIconVisible = true
         )
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Spacer(modifier = Modifier.weight(1f))
+            IconButton(onClick = {}) {
+                Icon(imageVector = Icons.Default.Refresh, contentDescription = "Refresh Button", tint = Color.Gray)
+            }
+        }
+        LazyColumn {
+            val stations = bookmarkScreenState.bookmarks.toList()
+            items(items = stations, key = { it }) { station ->
+                val response = bookmarkScreenState.arrivalTimeMap[station]
+                val secondText = response?.realtimeArrivalList?.firstOrNull()?.recptnDt
+                    ?: "도착 정보 없음"
+                CommonStationCard(
+                    primaryText = station,
+                    secondText = secondText,
+                    expandableIconVisible = true
+                )
+            }
+        }
     }
 }
